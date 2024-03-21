e52598d08b195df9dfdc514947b015188a4d4bb1
hiropino1515 <hiropino1515@gmail.com>
2024-03-22 06:06:22 +0900

    sp-navの重複を修正

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -35,68 +35,16 @@
 
       <nav class="header__sp-nav sp-nav js-sp-nav">
         <div class="sp-nav__inner">
-        <?php
-          wp_nav_menu(
-            array(
-              'depth' => '2',
-              'theme_location' => 'drawer',
-              'container' => 'false',
-              'menu_class' => 'sp-nav__items',
-            )
+          <?php
+            wp_nav_menu(
+              array(
+                'depth' => '2',
+                'theme_location' => 'drawer',
+                'container' => 'false',
+                'menu_class' => 'sp-nav__items',
+              )
             );
          ?>
-          <ul class="sp-nav__items">
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/campaign/' ) );?>">キャンペーン</a>
-              <ul>
-                <li>
-                <?php
-                  $terms = get_terms('campaign_category');
-                  foreach ( $terms as $term ) {
-                  echo '<a href="'.get_term_link($term).'">'.esc_html($term->name).'</a>';
-                  }
-                ?>
-                </li>
-              </ul>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/about/' ) );?>">私たちについて</a>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/information/' ) );?>">ダイビング情報</a>
-              <ul>
-                <li><a href="<?php echo esc_url( home_url( '/information?tab=license' ) );?>">ライセンス講習</a></li>
-                <li><a href="<?php echo esc_url( home_url( '/information?tab=diving' ) );?>">体験ダイビング</a></li>
-                <li><a href="<?php echo esc_url( home_url( '/information?tab=fundiving' ) );?>">ファンダイビング</a></li>
-              </ul>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/blog/' ) );?>">ブログ</a>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/voice/' ) );?>">お客様の声</a>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/price/' ) );?>">料金一覧</a>
-              <ul>
-                <li><a href="<?php echo esc_url( home_url( '/price#price1' ) );?>">ライセンス講習</a></li>
-                <li><a href="<?php echo esc_url( home_url( '/price#price2' ) );?>">体験ダイビング</a></li>
-                <li><a href="<?php echo esc_url( home_url( '/price#price3' ) );?>">ファンダイビング</a></li>
-              </ul>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/faq/' ) );?>">よくある質問</a>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/privacy-policy/' ) );?>">プライバシー <br>ポリシー</a>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/terms-of-service/' ) );?>">利用規約</a>
-            </li>
-            <li class="sp-nav__item">
-              <a href="<?php echo esc_url( home_url( '/contact/' ) );?>">お問い合せ</a>
-            </li>
-          </ul>
         </div>
       </nav>
     </div>
