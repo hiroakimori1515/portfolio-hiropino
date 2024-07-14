fcd6ce9f14dd76b8844142ba90bbef828bdbfeea
hiroakimori1515 <hiropino1515@gmail.com>
2024-07-14 11:08:08 +0900

    会員ログインページの追加

---
+++ WordPressTheme/page-membership-login.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<!-- 下層ページのメインビュー -->
+<div class="sub-mv">
+    <div class="sub-mv__inner">
+      <picture>
+        <source srcset="<?php echo get_theme_file_uri(); ?>/assets/images/common/sitemap-img_sp.jpg" media="(max-width: 767px)" >
+        <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/sitemap-img_pc.jpg" alt="上空から見たゴルフコースの全貌">
+      </picture>
+      <div class="sub-mv__header js-mv-header">
+        <h2 class="sub-mv__title" id="text">会員ログイン</h2>
+      </div>
+    </div>
+  </div>
+   <!-- パンくず -->
+   <?php get_template_part('parts/breadcrumb') ?>
+
+  <main>
+    <section class="page-membership-form layout-page-sitemap">
+      <div class="page-membership-form__inner inner">
+        <?php echo do_shortcode('[swpm_login_form]'); ?>
+      </div>
+    </section>
+
+<?php get_footer(); ?>
